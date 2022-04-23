a8bd5c9683164273231ecce7c376f87f12f372c7
adamteng <656081024@qq.com>
2022-04-23 23:29:13 +0800

    format clock

--- src/components/clock.vue
+++ src/components/clock.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+const start = new Date()
 const now = $(useNow())
+const timerMS = $computed(() => Math.round((+now - +start) / 1000))
+function formatTime(value: number) {
+  const result = value % (30 * 24 * 6)
+  const d = Math.floor(result / (6 * 24)) < 10 ? `0${Math.floor(result / (6 * 24))}` : Math.floor(result / (6 * 24))
+  const h = Math.floor((result / 6) % 24) < 10 ? `0${Math.floor((result / 6) % 24)}` : Math.floor((result / 6) % 24)
+  const m = Math.floor(result % 6)
+  return `June ${d} ${h}:${m}0`
+}
+const timeText = computed(() => formatTime(timerMS))
 </script>
 <template>
   <div>
-    {{ now }}
+    {{ timeText }}
   </div>
 </template>
